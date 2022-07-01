258720be0406481e60aa8af314da185a15eed424
Dima Grossman <dima@grossman.io>
2022-07-01 15:12:11 +0300

    feat: fix compilation destination

--- src/envirocheck.ts
+++ src/envirocheck.ts
@@ -1,26 +1,31 @@
-import * as fs from 'fs';
+import * as fs from 'fs'
 import { parse, stringify } from 'yaml'
-import { DependencyCheckerBase } from './plugins/base-checker';
-import { MongoChecker } from './plugins/mongo';
-import { RedisChecker } from './plugins/redis';
+import { DependencyCheckerBase } from './plugins/base-checker'
+import { MongoChecker } from './plugins/mongo'
+import { RedisChecker } from './plugins/redis'
 
 const plugins: { [key: string]: DependencyCheckerBase } = {
-  'redis': new RedisChecker(),
-  'mongodb': new MongoChecker()
-};
+  redis: new RedisChecker(),
+  mongodb: new MongoChecker()
+}
 
-(async () => {
-  const environment = fs.readFileSync('.envirocheck.yml', 'utf8');
-  const { dependencies } = parse(environment);
+;(async () => {
+  const isEnvironmentConfigFileExists = fs.existsSync('.envirocheck.yml')
+  if (!isEnvironmentConfigFileExists) {
+    throw new Error("Can't find .envirocheck.yml file")
+  }
+
+  const environment = fs.readFileSync('.envirocheck.yml', 'utf8')
+  const { dependencies } = parse(environment)
 
   for (const dependency of dependencies) {
-    const plugin = plugins[dependency] ;
+    const plugin = plugins[dependency]
 
     if (plugin) {
-      const result = await plugin.check();
+      const result = await plugin.check()
 
       if (!result) {
-        console.log(`${dependency} is not running`);
+        console.log(`${dependency} is not running`)
       }
     }
   }
