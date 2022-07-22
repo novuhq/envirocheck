bf6342f4a1f9c40bf87e1e2e0c8a06c29b537310
Gosha <djabarov.george@gmail.com>
2022-07-22 16:26:10 +0300

    refactor: class structure and logs styles

--- src/plugins/mongo/index.ts
+++ src/plugins/mongo/index.ts
@@ -1,14 +1,17 @@
-import { DependencyCheckerBase } from '../base-checker';
+import { DependencyCheckerBase } from '../base-checker'
 
 export class MongoChecker extends DependencyCheckerBase {
+  constructor() {
+    super(27018, 'mongodb')
+  }
+
   async check(): Promise<boolean> {
     try {
-      const inUse = await this.checkPort(27017);
-
-      return inUse;
+      return super.check()
     } catch (e) {
-      console.error(e);
-      return false;
+      console.error(e)
+
+      return false
     }
   }
 }
